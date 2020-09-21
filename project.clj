2c4c04d660aed43d728b80cf4753e9b65874fdfc
Daniel Ciumberică <ciumbi.ionut@gmail.com>
2020-09-20 23:16:05 -0400

    Updated uninstall script to avoid removing other installed plugins.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojupyter-plugin/lein-template "0.1.0"
+(defproject clojupyter-plugin/lein-template "0.1.1-SNAPSHOT"
   :description "Clojupyter plugin template for Leiningen"
   :url "https://github.com/nighcoder/clojupyter-plugin"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
